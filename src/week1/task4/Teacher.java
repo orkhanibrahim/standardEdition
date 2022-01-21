392dcffe2d69210c559c42b24d02baf018e7d508
orkhankhazar@gmail.com <Orxan.Ibrahimov@kapitalbank.az>
2022-01-22 01:19:05 +0400

    .

--- src/week1/task4/Teacher.java
+++ src/week1/task4/Teacher.java
@@ -1,10 +1,12 @@
 package week1.task4;
 
+import java.util.List;
+
 public class Teacher {
     private String name;
-    private Student student;
+    private List<Student> student;
 
-    public Teacher(String name, Student student){
+    public Teacher(String name, List<Student> student){
         this.name = name;
         this.student = student;
     }
