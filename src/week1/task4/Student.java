1dc77458a5f3db7f648f6d4a91f93baaf56277a0
orkhankhazar@gmail.com <Orxan.Ibrahimov@kapitalbank.az>
2022-01-22 01:27:35 +0400

    updated with list of students

--- src/week1/task4/Student.java
+++ src/week1/task4/Student.java
@@ -2,16 +2,14 @@ package week1.task4;
 
 public class Student {
     private String name;
-    private String surname;
     private int age;
 
-    public Student(String name, String surname, int age){
+    public Student(String name, int age){
         this.name = name;
-        this.surname = surname;
         this.age = age;
     }
 
     public String toString(){
-        return " Student : { " + name + " : " + surname + " : " + age + "}";
+        return " Student : { " + name + " : " + age + "}";
     }
 }
