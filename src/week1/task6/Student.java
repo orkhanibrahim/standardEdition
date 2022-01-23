dcff9140956b2f24417095470ee944e4cfe6bdb0
orkhankhazar@gmail.com <Orxan.Ibrahimov@kapitalbank.az>
2022-01-23 11:33:35 +0400

    task 6

---
+++ src/week1/task6/Student.java
@@ -0,0 +1,17 @@
+package week1.task6;
+
+import java.util.List;
+
+/*
+* List stundent olsun,studentin totalPoint adinda bir dene qiymeti olsun,
+* yeni method yaz, hemen method list studenti gonderende, studentin adi
+* ve qaabaginda qiymetine gore derecesi dushsun. 90> elaci; 80>90 zerbeci;
+*  70>80a normalci; 60>70 olmadikici, 60< ayibci*/
+public class Student {
+    private String name;
+    private double totalPoint;
+
+    public void showStudentNameAndDegree(List<Student> studentList){
+
+    }
+}
