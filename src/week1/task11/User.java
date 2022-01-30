45ec6c8ead90d4fe8b87d2b4f366b6526ada89ca
orxanibrahim <orkhankhazar@gmail.com>
2022-01-31 00:56:43 +0400

    class created

---
+++ src/week1/task11/User.java
@@ -0,0 +1,16 @@
+package week1.task11;
+/**
+ * Task 11: password validation: bir dene User olsun, ad soyad yas ve password olsun. bir dene method yaz, passworda
+ * validation olsun. Password 8 characterden yuxari olmalidi, 1dene boyuk, 1dene kicik, bir dene reqem olmalidi en azi.
+ * Passwordun ichinde adin ilk 4herfi olmamalidi, ve yashi olmamalidi.*/
+
+public class User {
+    private String name;
+    private String surname;
+    private int age;
+    private char[] password;
+
+    void passWordValidation(){
+
+    }
+}
